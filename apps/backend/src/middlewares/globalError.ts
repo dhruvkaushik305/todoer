7bab4c9a340fbf50a5eb5d1357c5bf26bd548a6a
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-04-19 12:43:52 +0530

    Added error catches

---
+++ apps/backend/src/middlewares/globalError.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+const GlobalError = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(err.message);
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+};
+export default GlobalError;
--- apps/backend/src/routes/route.ts
+++ apps/backend/src/routes/route.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import authRouter from "./auth";
+import GlobalError from "../middlewares/globalError";
 const router = express.Router();
 router.use("/auth", authRouter);
+router.use(GlobalError);
 export default router;

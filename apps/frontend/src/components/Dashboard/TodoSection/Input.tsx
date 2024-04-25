74a889c8f603a9714479d657aceebfe1a0c65758
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-04-25 16:31:24 +0530

    Connected and tested the create todo

--- apps/frontend/src/components/Dashboard/TodoSection/Input.tsx
+++ apps/frontend/src/components/Dashboard/TodoSection/Input.tsx
@@ -1,16 +1,59 @@
 import React, { useRef } from "react";
+import { useSetRecoilState } from "recoil";
+import todoSelector from "../../../store/todo";
+import { TodoType } from "@repo/types/Todo";
+import { toast } from "sonner";
 const Input: React.FC = () => {
+    const setTodos = useSetRecoilState(todoSelector);
     const task = useRef<HTMLInputElement>(null);
-    const handleAddTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleAddTodo = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (task.current && task.current.value.trim() !== ""){
-            console.log(task.current.value);
+        if (task.current && task.current.value.trim() !== "") {
+            setTodos((oldTodos) => {
+                if (oldTodos) {
+                    return [...oldTodos, {
+                        id: Math.random().toString(36).substring(2, 9),
+                        order: oldTodos.length,
+                        task: task.current!.value,
+                        completed: false,
+                        userId: "1"
+                    } as TodoType
+                    ]
+                } else return [];
+            })
+            const res = await fetch(`${import.meta.env.VITE_BACKEND}/todo/create`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    task: task.current.value,
+                }),
+                credentials: "include"
+            });
+            const response = await res.json();
+            if (response.success) {
+                const newTodo: TodoType = response.data;
+                setTodos((oldTodos) => {
+                    if (oldTodos) {
+                        return oldTodos.map((todo) => {
+                            if (todo.userId === "1") {
+                                return newTodo;
+                            } else return todo;
+                        })
+                    } else return [];
+                })
+                toast.success("Todo saved");
+            }
+            else {
+                toast.error("Failed to save todo");
+            }
             task.current.value = "";
         }
     }
     return <div className=" p-2">
         <form className="flex gap-2" onSubmit={handleAddTodo}>
-            <input type="text" placeholder="What're you doing today?"className="p-2 rounded-md w-full focus:outline-none text-xl border-2" ref={task}/>
+            <input type="text" placeholder="What're you doing today?" className="p-2 rounded-md w-full focus:outline-none text-xl border-2" ref={task} />
             <button className="p-2 px-3 text-white rounded-md transition ease-in-out bg-black hover:bg-gray-700 hover:scale-90 duration-700">Add</button>
         </form>
     </div>;

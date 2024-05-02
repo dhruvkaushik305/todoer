3e205ba7fb820cdef6f96a71124f8d786c4072a1
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-05-02 12:31:38 +0530

    FEAT: implemented the user actions and click handlers to follow and unfollow a user

--- apps/frontend/src/actions/userActions.ts
+++ apps/frontend/src/actions/userActions.ts
@@ -33,3 +33,41 @@ export const SearchUser = async (username: string) => {
     return { success: false };
   }
 };
+export const FollowUser = async (userId: string) => {
+  try {
+    const res = await fetch(
+      `${import.meta.env.VITE_BACKEND}/user/follow/${userId}`,
+      {
+        credentials: "include",
+      }
+    );
+    const response: { success: boolean; message?: string; error?: string } =
+      await res.json();
+    return response;
+  } catch (err) {
+    console.error("Error while following the user", err);
+    return {
+      success: false,
+      error: "Something went wrong while making the request",
+    };
+  }
+};
+export const UnfollowUser = async (userId: string) => {
+  try {
+    const res = await fetch(
+      `${import.meta.env.VITE_BACKEND}/user/unfollow/${userId}`,
+      {
+        credentials: "include",
+      }
+    );
+    const response: { success: boolean; message?: string; error?: string } =
+      await res.json();
+    return response;
+  } catch (err) {
+    console.error("Error while unfollowing the user", err);
+    return {
+      success: false,
+      error: "Something went wrong while making the request",
+    };
+  }
+};
--- apps/frontend/src/components/Home/SocialsSection/User.tsx
+++ apps/frontend/src/components/Home/SocialsSection/User.tsx
@@ -1,19 +1,41 @@
 import { UserType } from "@repo/types/User";
-import React from "react";
+import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import selectedUserAtom from "../../../store/user";
+import { FollowUser, UnfollowUser } from "../../../actions/userActions";
 interface UserProps {
     user: UserType;
 }
 const User: React.FC<UserProps> = (props) => {
+    const [follow, setFollow] = useState(false);
+    const [disabled, setDisabled] = useState(false);
     const setSelectedUser = useSetRecoilState(selectedUserAtom);
     const { user } = props;
     const handleClick = async () => {
         setSelectedUser(user);
     }
-    return <div className="bg-gray-200 m-2 p-4 rounded-md cursor-pointer" onClick={handleClick}>
-        <p className="text-2xl">{user.name}</p>
-        <p className="text-sm">{`@${user.username}`}</p>
+    const followHandler = async () => {
+        setDisabled(true);
+        const response = await FollowUser(user.id);
+        if (response.success) {
+            setFollow(true);
+        }
+        setDisabled(false);
+    }
+    const unfollowHandler = async () => {
+        setDisabled(true);
+        const response = await UnfollowUser(user.id);
+        if (response.success) {
+            setFollow(false);
+        }
+        setDisabled(false);
+    }
+    return <div className="bg-gray-200 m-2 p-4 rounded-md flex justify-between items-center" >
+        <div className="cursor-pointer" onClick={handleClick}>
+            <p className="text-2xl">{user.name}</p>
+            <p className="text-sm">{`@${user.username}`}</p>
+        </div>
+        {follow ? <button className="px-3 py-2 rounded md border-2 border-black" onClick={unfollowHandler} disabled={disabled}>Unfollow</button> : <button className="px-3 py-2 bg-black rounded-md text-white" onClick={followHandler} disabled={disabled}>Follow</button>}
     </div>
 }
 export default User;

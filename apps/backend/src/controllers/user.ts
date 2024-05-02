8651ec10f112430a111a9e4de92cc581505e51f0
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-05-02 12:05:59 +0530

    FEAT: Added endpoints to follow and unfollow a user

--- apps/backend/src/controllers/user.ts
+++ apps/backend/src/controllers/user.ts
@@ -4,7 +4,6 @@ import db from "@repo/db/prisma";
 interface userRequest extends Request {
   user?: UserType;
 }
-//TODO: Should not return the user itself
 export const searchUsers = async (
   req: userRequest,
   res: Response,
@@ -17,7 +16,6 @@ export const searchUsers = async (
       .json({ success: false, message: "Incomplete request" });
   }
   try {
-    //remove the user from the search results
     const users = await db.user.findMany({
       where: {
         username: { contains: query.toString() },
@@ -30,6 +28,89 @@ export const searchUsers = async (
         .json({ success: false, message: "No users found" });
     }
     return res.status(200).json({ success: true, data: users });
+  } catch (err) {
+    next(err);
+  }
+};
+export const followUser = async (
+  req: userRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req.params;
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Incomplete request" });
+  }
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const isFollowing = await db.follow.findFirst({
+      where: {
+        followerId: userId,
+        followingId: req.user?.id,
+      },
+    });
+    if (isFollowing) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Already following user" });
+    }
+    await db.follow.create({
+      data: {
+        followerId: userId,
+        followingId: req.user!.id,
+      },
+    });
+    return res.status(200).json({ success: true, message: "Followed user" });
+  } catch (err) {
+    next(err);
+  }
+};
+export const unfollowUser = async (
+  req: userRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req.params;
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Incomplete request" });
+  }
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const isFollowing = await db.follow.findFirst({
+      where: {
+        followerId: userId,
+        followingId: req.user?.id,
+      },
+    });
+    if (!isFollowing) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User does not follow this account" });
+    }
+    await db.follow.delete({
+      where: {
+        id: isFollowing.id,
+      },
+    });
+    return res.status(200).json({ success: true, message: "Unfollowed user" });
   } catch (err) {
     next(err);
   }
--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { searchUsers } from "../controllers/user";
+import { followUser, searchUsers, unfollowUser } from "../controllers/user";
 import authenticateUser from "../middlewares/auth";
 const router = express.Router();
 router.use(authenticateUser);
 router.get("/search", searchUsers);
+router.get("/follow/:userId", followUser);
+router.get("/unfollow/:userId", unfollowUser);
 export default router;

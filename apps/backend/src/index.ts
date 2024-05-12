08d366c8177c217dbadfd93934601416df9ac799
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-05-12 16:12:31 +0530

    FIX: Replaced express json middlewares with bodyparser

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import apiV1 from "./routes/v1Router";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:5173",

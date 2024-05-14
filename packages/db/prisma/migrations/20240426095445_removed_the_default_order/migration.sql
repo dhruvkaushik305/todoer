8c70addbb11e7c2f011e80eba7e3b11ebb8c4454
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-05-14 17:35:09 +0530

    FIX: Solved "following" bugs from the schema

--- packages/db/prisma/migrations/20240426095445_removed_the_default_order/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropIndex
-DROP INDEX "Todo_order_key";
-
--- AlterTable
-ALTER TABLE "Todo" ALTER COLUMN "order" DROP DEFAULT;
-DROP SEQUENCE "Todo_order_seq";
--- packages/db/prisma/migrations/20240514110955_init/migration.sql
+++ packages/db/prisma/migrations/20240514110955_init/migration.sql
@@ -13,6 +13,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Todo" (
     "id" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
     "task" TEXT NOT NULL,
     "completed" BOOLEAN NOT NULL DEFAULT false,
     "userId" TEXT NOT NULL,
@@ -21,11 +22,33 @@ CREATE TABLE "Todo" (
     CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Follow" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "followedById" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Follow_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Follow_userId_key" ON "Follow"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Follow_followedById_key" ON "Follow"("followedById");
+
 -- AddForeignKey
 ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followedById_fkey" FOREIGN KEY ("followedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

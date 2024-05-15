c957250dc7ad6ebb6fed9a60cd3d60358b8c2076
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-05-15 11:15:18 +0530

    FEAT: Added the landing page with responsiveness
    Also addresses minor bugs like navigating to / when clicked on the logo and altering the font sizes of the buttons

--- apps/frontend/src/components/Navbar/NavbarLayout.tsx
+++ apps/frontend/src/components/Navbar/NavbarLayout.tsx
@@ -9,21 +9,22 @@ const NavbarLayout: React.FC = () => {
   const [user, setUser] = useRecoilState(userData);
   return (
     <div className="fixed w-full flex bg-black justify-between md:justify-around p-3 items-center border-b-2 border-gray-700">
-      <div className="md:text-4xl text-3xl text-white font-Pacifico">
+      <div className="md:text-4xl text-3xl text-white font-Pacifico cursor-pointer" onClick={() => navigate("/")}>
         Todoer
       </div>
       {user === null ? (<div className="flex justify-between gap-3">
         <button
-          className="md:text-xl text-lg bg-blue text-white md:py-2 md:px-3 py-1 px-2 rounded-lg"
+          className="md:text-lg text-md bg-blue text-white md:py-2 md:px-3 py-1 px-2 rounded-lg"
           onClick={() => navigate("/auth/signup")}
         >
           Signup
         </button>
-        <button className="md:text-xl text-lg bg-blue text-white md:py-2 md:px-3 py-1 px-2 rounded-lg" onClick={() => navigate("/auth/login")}>
+        <button className="md:text-lg text-md bg-blue text-white md:py-2 md:px-3 py-1 px-2 rounded-lg" onClick={() => navigate("/auth/login")}>
           Login
         </button>
       </div>) : (<div>
         <button className="bg-blue rounded-md text-white px-3 py-2" onClick={async () => {
+          // TODO: Move this to actions
           const res = await fetch(`${import.meta.env.VITE_BACKEND}/auth/logout`, {
             method: "DELETE",
             credentials: "include"

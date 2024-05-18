dd267008dd7dc70220b5f9e6837a65c71fc4f9f4
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-05-18 19:48:08 +0530

    FIX: Added the secure attribute to the cookie

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -80,6 +80,7 @@ export async function login(req: Request, res: Response, next: NextFunction) {
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
       sameSite: "none",
+      secure: true,
     });
     return res.status(200).json({ success: true, data: user });
   } catch (err) {

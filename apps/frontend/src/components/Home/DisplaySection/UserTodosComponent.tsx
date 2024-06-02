243d2354453fbc0318e982512c7d3c56a367bcf1
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-06-02 13:09:10 +0530

    FIX: Removed the components folder

--- apps/frontend/src/components/Home/DisplaySection/UserTodosComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TodoListComponent from "./TodoListComponent";
-const UserTodosComponent: React.FC = () => {
-    return <div>
-        <TodoListComponent />
-    </div>
-}
-export default UserTodosComponent;

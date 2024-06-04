da40b25a51045f690491bb85623fe27e2e644b56
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-06-04 09:22:35 +0530

    FEAT: Added confetti animations

--- apps/frontend/src/Layouts/ProgressBarLayout.tsx
+++ apps/frontend/src/Layouts/ProgressBarLayout.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import { getPercentageCompletedSelector } from "../store/todo";
+import confetti from "canvas-confetti";
 const ProgressBarLayout: React.FC = () => {
   const percentageCompleted = useRecoilValue(getPercentageCompletedSelector);
+  useEffect(() => {
+    if (percentageCompleted == 100) {
+      const end = Date.now() + 1 * 1000;
+      const colors = ["#22c55e", "#d1d5db"];
+      (function frame() {
+        confetti({
+          particleCount: 7,
+          angle: 60,
+          spread: 50,
+          origin: { x: 0 },
+          colors: colors,
+          gravity: 2,
+        });
+        confetti({
+          particleCount: 7,
+          angle: 120,
+          spread: 50,
+          origin: { x: 1 },
+          colors: colors,
+          gravity: 2,
+        });
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+    }
+  }, [percentageCompleted]);
+
   const percentage = parseFloat(percentageCompleted.toFixed(0));
   return (
     <div className="flex w-full items-center gap-2">
